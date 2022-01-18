c6dacc6f289ca4d3859ac6db5528455b96b5a3e1
Fedor Babikov <fed.babikov@protonmail.com>
2022-01-18 15:16:38 +0100

    finished ex-3: the code works in all browsers console, but the unit tests here give errors

--- 2-Browsers/Week1/homework/ex3-hijackLogo.js
+++ 2-Browsers/Week1/homework/ex3-hijackLogo.js
@@ -7,8 +7,18 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 2. Modify the `src` and `srcset` of the logo so that it's replaced by the 
    HackYourFuture logo instead.
 ------------------------------------------------------------------------------*/
+
+// This code works in all browsers' console, but the unit tests here give errors...
 function hijackGoogleLogo() {
-  // TODO your code goes in here
+  const logo = document.querySelector('img.lnXdpd');
+  logo.setAttribute(
+    'src',
+    'https://www.hackyourfuture.dk/static/logo-dark.svg'
+  );
+  logo.setAttribute(
+    'srcset',
+    'https://www.hackyourfuture.dk/static/logo-dark.svg'
+  );
 }
 
 hijackGoogleLogo();
