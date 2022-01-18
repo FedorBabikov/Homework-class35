04089dfaea3eb362748d17faf17ccb98e78a3dea
Fedor Babikov <fed.babikov@protonmail.com>
2022-01-18 14:34:39 +0100

    finished ex-2

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -9,5 +9,14 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 3. In JavaScript, iterate through each `<li>` and change the class to 
    `list-item`.
 ------------------------------------------------------------------------------*/
+document.querySelector('body').style.fontFamily = 'Arial, sans-serif';
+// object with personal info
+const info = { nickname: 'Batman', 'fav-food': 'pizza', hometown: 'Amsterdam' };
+// take all the li's from document
+const liSet = document.querySelectorAll('li');
 
-// TODO add your JavaScript code here.
+for (const li of liSet) {
+  li.classList.add('list-item');
+
+  li.children[0].textContent = info[li.children[0].id];
+}
